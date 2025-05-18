7e0bd91493daf9e69a8bd602c42fc4dbd7bfc5f5
Mikael <mikael.hatanpaa@gmail.com>
2025-05-18 14:58:32 +0200

    Added simple validation to book data model

--- models/book.go
+++ models/book.go
@@ -1,5 +1,9 @@
 package models
 
+import (
+	"strings"
+)
+
 type BookStatus string
 
 const (
@@ -9,8 +13,34 @@ const (
 )
 
 type Book struct {
-	ID     string
-	Title  string
-	Author string
-	Status BookStatus
+	ID     string     `json:"id"`
+	Title  string     `json:"title"`
+	Author string     `json:"author"`
+	Status BookStatus `json:"status"`
+}
+
+func (b *Book) Validate() error {
+	b.Title = strings.TrimSpace(b.Title)
+	b.Author = strings.TrimSpace(b.Author)
+
+	trimmedStatus := strings.TrimSpace(string(b.Status))
+	lowercaseStatus := strings.ToLower(trimmedStatus)
+	status := BookStatus(lowercaseStatus)
+
+	if b.Title == "" {
+		//ERROR MISSING TITLE
+	}
+
+	if b.Author == "" {
+		// ERROR MISSING AUTHOR
+	}
+
+	switch status {
+	case BookUnread, BookReading, BookComplete:
+		// ALL GOOD
+	default:
+		// ERROR UNKNOWN STATUS
+	}
+
+	return nil
 }

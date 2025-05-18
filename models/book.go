366b7f71160c69d251bb40aa002d064f17925b87
Mikael <mikael.hatanpaa@gmail.com>
2025-05-18 14:40:55 +0200

    Added custom enum model for a books reading status

--- models/book.go
+++ models/book.go
@@ -1,8 +1,16 @@
 package models
 
+type BookStatus string
+
+const (
+	BookUnread   BookStatus = "unread"
+	BookReading  BookStatus = "reading"
+	BookComplete BookStatus = "complete"
+)
+
 type Book struct {
 	ID     string
 	Title  string
 	Author string
-	Status string
+	Status BookStatus
 }
